dd2bb935a7e7c58d821399398c6565b4d4637001
Katherine Bielawiec <kbielawi@ucsd.edu>
2016-01-19 11:04:29 -0800

    Added function name/call

--- pa2/problem21_1.py
+++ pa2/problem21_1.py
@@ -12,34 +12,37 @@ Katherine Bielawiec, kbielawi@ucsd.edu
 
 import fileinput
 
-CLEAN = 0
-DIRTY = 1
-NUMROOM = 1
+def check_goal():
+    CLEAN = 0
+    DIRTY = 1
+    NUMROOM = 1
 
 
-for line in fileinput.input():
-    params = line.replace(' ','').replace('\n', '').split(',') # Get rid of whitespace and create list of input
+    for line in fileinput.input():
+        params = line.replace(' ','').replace('\n', '').split(',') # Get rid of whitespace and create list of input
 
-    for num in range (0,NUMROOM+2):                                  #cast all to ints
-        params[num] = int(params[num])
+        for num in range (0,NUMROOM+2):                                  #cast all to ints
+            params[num] = int(params[num])
 
-    #print(params[0], params[1], params[2])
-    # Check if all input is valid
+        #print(params[0], params[1], params[2])
+        # Check if all input is valid
 
-    for num in params:
-        # room number invalid
-        if params[NUMROOM+1]>NUMROOM:
-            print("invalid input")
-            break
-        # room dirty/clean invalid
-        if ((params[0] !=0 and params[0] !=1) or (params[1] != 0 and params[1] != 1)):
-            print ("invalid input")
-            break
-        #both rooms clean
-        elif (params[0] == 0 and params[1] == 0):
-            print ("True")
-            break
-        # at least one room dirty
-        else:
-            print ("False")
-            break
+        for num in params:
+            # room number invalid
+            if params[NUMROOM+1]>NUMROOM:
+                print("invalid input")
+                break
+            # room dirty/clean invalid
+            if ((params[0] !=0 and params[0] !=1) or (params[1] != 0 and params[1] != 1)):
+                print ("invalid input")
+                break
+            #both rooms clean
+            elif (params[0] == 0 and params[1] == 0):
+                print ("True")
+                break
+            # at least one room dirty
+            else:
+                print ("False")
+                break
+
+check_goal()
